1c93ce9ebaeb6a7bc22c011ec0149e83907369a9
El Senpai <El Senpai@EL-SENPAI>
2021-03-11 14:36:35 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import{HttpClientModule} from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { CategoryComponent } from './components/category/category.component';
+import { NaviComponent } from './components/navi/navi.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    CategoryComponent,
+    NaviComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/models/productResponseModel.ts
@@ -0,0 +1,7 @@
+import { ResponseModel } from "./responseModel";
+import {Product} from "./product";
+
+export interface ProductResponseModel extends ResponseModel{
+    data:Product[]
+  
+}
